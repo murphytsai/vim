72a276a21863de16f27a7c6f820dfd187a2912ee
yft <murphy@askey.com.tw>
2011-01-27 20:45:37 +0800

    Add: thinkpad-keyboard.sh

---
+++ thinkpad-keyboard.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xinput set-int-prop 10 "Evdev Wheel Emulation" 8 1
+xinput set-int-prop 10 "Evdev Wheel Emulation Button" 8 2
+xinput set-int-prop 10 "Evdev Wheel Emulation Timeout" 8 200
+xinput set-int-prop 10 "Evdev Wheel Emulation Axes" 8 6 7 4 5
